241f2bf7dd46d8a79a0b63598a732bc56441dd02
cmmerritt <christiane.merritt@gmail.com>
2021-03-26 13:53:09 -0700

    refactor high low guess text display

--- utils.js
+++ utils.js
@@ -4,3 +4,14 @@ export function compareNumbers(guess, correctNumber) {
     if (guess > correctNumber) return 1;
 }
 
+export function getHighOrLowText(comparisonResult) {
+    if (comparisonResult === 0) return '';
+    if (comparisonResult === -1) return 'Your guess is too low. Try again!';
+    if (comparisonResult === 1) return 'Your guess is too high. Try again!';
+}
+
+/* export function getPlantHealthText(plantHealth) {
+    if (plantHealth === 'dry') return 'Your plant needs water!';
+    if (plantHealth === 'healthy') return 'Your plant is a-ok!';
+    
+    return 'Your plant is drowining!'; */
